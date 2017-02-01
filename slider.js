0e003abdb35df7a1a25f3776360cb1fda6f43721
ewelina <skibinska.ewelina@gmail.com>
2017-02-01 16:39:55 +0000

    Rename class
    related #8

--- slider.js
+++ slider.js
@@ -1,20 +1,20 @@
 //lets populate reel numbers
-var min = $(".rangeslider").attr("min");
-var max = $(".rangeslider").attr("max");
+var min = $(".slider__range").attr("min");
+var max = $(".slider__range").attr("max");
 
-updateOutput($(".rangeslider").val());
+updateOutput($(".slider__range").val());
 
 var rfigure, h;
 function updateOutput(figure) {
 	//because of the step param the slider will return values in multiple of 0.05 so we have to round it up
 	rfigure = Math.round(figure);
 	//displaying the static output
-	$(".static-output").html(rfigure);
+	$(".slider__banana").html(rfigure);
 
 	//positioning .static-output and .reel
 	//horizontal positioning first
-	h = figure/max*($(".input-wrapper").width()-$(".reel").width()) + 'px';
+	h = figure/max*($(".eating__slider").width()-$(".slider__reel").width()) + 'px';
 
 	//applying the positions
-	$(".static-output, .reel").css({left: h});
+	$(".slider__banana, .slider__reel").css({left: h});
 }
