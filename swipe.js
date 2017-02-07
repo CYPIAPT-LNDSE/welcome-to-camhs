df3355135b43424670472e84c04ae74a3bf8aeb3
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-02-07 22:26:56 +0000

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,48 +1,48 @@
 function swipe(direction){
-    var endpoint = window.location.href.split('/')[7];
+    var endpoint = window.location.pathname;
     var forward, backward;
 
-    // Homepage to choose an avatar page
-    if (endpoint === 'index.html'){
-          forward = 'choose-an-avatar.html',
-          backward = ''
-    };
+    // Homepage to avatar page
+    if (endpoint === "/welcome-to-camhs/"){
+          forward = "https://cypiapt-lndse.github.io/welcome-to-camhs/choose-an-avatar";
+          backward = "https://cypiapt-lndse.github.io/welcome-to-camhs";
+    }
 
     // Avatar to introduction page
-    if (endpoint === 'choose-an-avatar.html'){
-        forward = 'introduction.html',
-        backward = 'index.html'
-    };
+    if (endpoint === '/welcome-to-camhs/choose-an-avatar'){
+        forward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/introduction';
+        backward = 'https://cypiapt-lndse.github.io/welcome-to-camhs';
+    }
 
-    // Introduction to feelings page
-    if (endpoint === 'introduction.html'){
-        forward = 'eating.html',
-        backward = 'choose-an-avatar.html'
-    };
+    // Introduction to eating page
+    if (endpoint === '/welcome-to-camhs/introduction'){
+        forward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/eating';
+        backward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/choose-an-avatar';
+    }
 
-    // Eating page to finish page
-    if (endpoint === 'eating.html'){
-        forward = 'feelings.html',
-        backward = 'introduction.html'
-    };
+    // Eating to personality page
+    if (endpoint === '/welcome-to-camhs/eating'){
+        forward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/personality';
+        backward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/introduction';
+    }
+    
+    // Personality to feelings page
+    if (endpoint === '/welcome-to-camhs/personality'){
+        forward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/feelings';
+        backward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/eating';
+    }
+    
+    // Feelings to finish page
+    if (endpoint === '/welcome-to-camhs/feelings'){
+        forward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/finished';
+        backward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/personality';
+    }
 
-    // Feelings page to eating page
-    if (endpoint === 'feelings.html'){
-        forward = 'personality.html',
-        backward = 'eating.html'
-    };
-
-    // Eating page to finish page
-    if (endpoint === 'personality.html'){
-        forward = 'finished.html',
-        backward = 'feelings.html'
-    };
-
-    // Eating page to finish page
-    if (endpoint === 'finished.html'){
-        forward = '',
-        backward = 'personality.html'
-    };
+    // Finish page
+    if (endpoint === '/welcome-to-camhs/finished'){
+        forward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/finished';
+        backward = 'https://cypiapt-lndse.github.io/welcome-to-camhs/feelings';
+    }
 
     if(direction === "swipeleft" && event.target.tagName !== 'IMG'){
       window.location.href = forward;
@@ -54,5 +54,5 @@ function swipe(direction){
 }
 
 // The if statement stops the carousel from triggering the page swipe
-Hammer(document.body).on("swipeleft", function(){ swipe("swipeleft") });
-Hammer(document.body).on("swiperight", function(){ swipe("swiperight") });
+Hammer(document.body).on("swipeleft", function(){ swipe("swipeleft"); });
+Hammer(document.body).on("swiperight", function(){ swipe("swiperight"); });
