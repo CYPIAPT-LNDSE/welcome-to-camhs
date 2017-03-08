ced165c65efbe61eae708f7a1b855eab82256289
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-03-08 14:22:56 +0000

    Add first test to server.js. Related #77
    We have added tap-spec as a dev-dependency.

---
+++ tests/server.test.js
@@ -0,0 +1,14 @@
+const test = require('tape');
+const server = require('../src/server');
+
+test('Check the index route', t => {
+  const options = {
+    method: 'GET',
+    url: '/'
+  };
+  server.inject(options, (response) => {
+    t.equal(response.statusCode, 200, 'You received a 200 status code, test passed');
+    // server.stop();
+    t.end();
+  });
+});
