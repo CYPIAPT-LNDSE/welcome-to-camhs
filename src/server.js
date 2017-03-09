2b1aab915595ba8d7626590cc62dc7709e36a426
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-03-09 10:16:36 +0000

    Capitalize Hapi, Inert, Vision and Path.
    Capitalizes all of the node/hapi modules we are
    requiring. Related #77.

--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
-const hapi = require('hapi');
-const inert = require('inert');
-const vision = require('vision');
-const routes = require('./routes');
-const path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const Path = require('path');
 
-const server = new hapi.Server();
+const routes = require('./routes');
+
+const server = new Hapi.Server();
 
 server.connection({
   port: process.env.PORT || 8080,
   routes: {
     files: {
-      relativeTo: path.join(__dirname, '..', 'public')
+      relativeTo: Path.join(__dirname, '..', 'public')
     }
   }
 });
 
-server.register([inert], err => {
+server.register([Inert], err => {
   if(err) console.log(err);
 
   server.route(routes);
