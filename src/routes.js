fc0a565e820a635ba57385020e928a9e31d00377
ewelina <skibinska.ewelina@gmail.com>
2017-03-09 16:28:38 +0000

    Create routes for each view
    related #82

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,55 @@
 module.exports = [
   {
     method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+      reply.view('welcome');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/avatar',
+    handler: (request, reply) => {
+      reply.view('choose-an-avatar');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/introduction',
+    handler: (request, reply) => {
+      reply.view('introduction');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/feelings',
+    handler: (request, reply) => {
+      reply.view('feelings');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/eating',
+    handler: (request, reply) => {
+      reply.view('eating');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/personality',
+    handler: (request, reply) => {
+      reply.view('personality');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/finished',
+    handler: (request, reply) => {
+      reply.view('finished');
+    }
+  },
+  {
+    method: 'GET',
     path: '/{file*}',
     handler: {
       directory: {
@@ -10,9 +59,11 @@ module.exports = [
   },
   {
     method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-      reply.view('welcome');
+    path: '/lib/{file*}',
+    handler: {
+      directory: {
+        path: 'lib'
+      }
     }
   }
 ];
