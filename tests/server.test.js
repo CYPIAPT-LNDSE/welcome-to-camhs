db2fac20b45dd5d8eda38942468c97c9fee16ad4
ewelina <skibinska.ewelina@gmail.com>
2017-03-10 12:13:52 +0000

    Add tests checking diffrent routes
    related #81

--- tests/server.test.js
+++ tests/server.test.js
@@ -33,3 +33,47 @@ test('Check /index.js', t => {
     t.end();
   });
 });
+
+test('Check /introduction', t => {
+  const options = {
+    method: 'GET',
+    url: '/introduction'
+  };
+  server.inject(options, response => {
+    t.equal(response.statusCode, 200, 'You received a 200 status code, test passed');
+    t.end();
+  });
+});
+
+test('Check /avatar', t => {
+  const options = {
+    method: 'GET',
+    url: '/avatar'
+  };
+  server.inject(options, response => {
+    t.equal(response.statusCode, 200, 'You received a 200 status code, test passed');
+    t.end();
+  });
+});
+
+test('Check /eating', t => {
+  const options = {
+    method: 'GET',
+    url: '/eating'
+  };
+  server.inject(options, response => {
+    t.equal(response.statusCode, 200, 'You received a 200 status code, test passed');
+    t.end();
+  });
+});
+
+test('Check /personality', t => {
+  const options = {
+    method: 'GET',
+    url: '/personality'
+  };
+  server.inject(options, response => {
+    t.equal(response.statusCode, 200, 'You received a 200 status code, test passed');
+    t.end();
+  });
+});
