b305bfa9d975b418545e80902e3b507b56cb4d2c
ewelina <skibinska.ewelina@gmail.com>
2017-03-13 14:59:46 +0000

    Connect pages, add buttons on pages
    related #88

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,21 @@ module.exports = [
     method: 'GET',
     path: '/',
     handler: (request, reply) => {
-      reply.view('welcome');
+      var data = {
+        next: '/avatar'
+      };
+      reply.view('welcome', data);
     }
   },
   {
     method: 'GET',
     path: '/avatar',
     handler: (request, reply) => {
-      var data = { avatar: true };
+      var data = {
+        avatar: true,
+        prev: '/',
+        next: '/introduction'
+       };
       reply.view('choose-an-avatar', data);
     }
   },
@@ -18,35 +25,54 @@ module.exports = [
     method: 'GET',
     path: '/introduction',
     handler: (request, reply) => {
-      reply.view('introduction');
-    }
-  },
-  {
-    method: 'GET',
-    path: '/feelings',
-    handler: (request, reply) => {
-      reply.view('feelings');
+      var data =  {
+        prev: '/avatar',
+        next: '/eating'
+      };
+      reply.view('introduction', data);
     }
   },
   {
     method: 'GET',
     path: '/eating',
     handler: (request, reply) => {
-      reply.view('eating');
+      var data =  {
+        prev: '/introduction',
+        next: '/feelings'
+      };
+      reply.view('eating', data);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/feelings',
+    handler: (request, reply) => {
+      var data =  {
+        prev: '/eating',
+        next: '/personality'
+      };
+      reply.view('feelings', data);
     }
   },
   {
     method: 'GET',
     path: '/personality',
     handler: (request, reply) => {
-      reply.view('personality');
+      var data =  {
+        prev: '/feelings',
+        next: '/finished'
+      };
+      reply.view('personality', data);
     }
   },
   {
     method: 'GET',
     path: '/finished',
     handler: (request, reply) => {
-      reply.view('finished');
+      var data =  {
+        prev: '/personality',
+      };
+      reply.view('finished', data);
     }
   },
   {
