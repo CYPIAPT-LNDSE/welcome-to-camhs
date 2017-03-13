700e7f9ec139697dbe7aa268087103727b2e9b74
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-03-13 10:46:42 +0000

    Add IIFE around index.html file. Related #58.
    Part of the process to refactor our js files into
    one file.

--- public/index.js
+++ public/index.js
@@ -1,88 +1,83 @@
-"use strict";
+(function () {
+  "use strict";
 
-$(document).ready(function(){
   $('.carousel').carousel({
     indicators: true,
     shift: 100
   });
-});
 
-var addAnswersToSessionStorage = function(){
-  var nameInput = document.getElementsByClassName("name-input")[0];
-  var ageInput = document.getElementsByClassName("age-input")[0];
-  var strange = document.getElementById("strange");
-  var happy = document.getElementById("happy");
-  var angry = document.getElementById("angry");
-  var fun = document.getElementById("fun");
-  var boring = document.getElementById("boring");
-  var kind = document.getElementById("kind");
-  var sad = document.getElementById("sad");
-  var happyEmoji = document.getElementsByClassName("emoji")[0];
-  var sadEmoji = document.getElementsByClassName("emoji")[1];
-  var indifferentEmoji = document.getElementsByClassName("emoji")[2];
-  var personality = [];
+  function addAnswersToSessionStorage(){
+    var nameInput = document.getElementsByClassName("name-input")[0];
+    var ageInput = document.getElementsByClassName("age-input")[0];
+    var strange = document.getElementById("strange");
+    var happy = document.getElementById("happy");
+    var angry = document.getElementById("angry");
+    var fun = document.getElementById("fun");
+    var boring = document.getElementById("boring");
+    var kind = document.getElementById("kind");
+    var sad = document.getElementById("sad");
+    var happyEmoji = document.getElementsByClassName("emoji")[0];
+    var sadEmoji = document.getElementsByClassName("emoji")[1];
+    var indifferentEmoji = document.getElementsByClassName("emoji")[2];
+    var personality = [];
 
-  addKeyupEvent('name', nameInput);
-  addKeyupEvent('age', ageInput);
+    addKeyupEvent('name', nameInput);
+    addKeyupEvent('age', ageInput);
 
-  addClickEventArray('personality', strange, 'strange', personality);
-  addClickEventArray('personality', happy, 'happy', personality);
-  addClickEventArray('personality', angry, 'angry', personality);
-  addClickEventArray('personality', fun, 'fun', personality);
-  addClickEventArray('personality', boring, 'boring', personality);
-  addClickEventArray('personality', kind, 'kind', personality);
-  addClickEventArray('personality', sad, 'sad', personality);
+    addClickEventArray('personality', strange, 'strange', personality);
+    addClickEventArray('personality', happy, 'happy', personality);
+    addClickEventArray('personality', angry, 'angry', personality);
+    addClickEventArray('personality', fun, 'fun', personality);
+    addClickEventArray('personality', boring, 'boring', personality);
+    addClickEventArray('personality', kind, 'kind', personality);
+    addClickEventArray('personality', sad, 'sad', personality);
 
-  addClickEventSingle('feelings', sadEmoji, 'sad');
-  addClickEventSingle('feelings', happyEmoji, 'happy');
-  addClickEventSingle('feelings', indifferentEmoji, 'indifferent');
+    addClickEventSingle('feelings', sadEmoji, 'sad');
+    addClickEventSingle('feelings', happyEmoji, 'happy');
+    addClickEventSingle('feelings', indifferentEmoji, 'indifferent');
 
-  function addKeyupEvent(key, element){
-    if(!element){ return; };
-    element.addEventListener("keyup", function(){
-      addToStorageValue(key, element.value)
-    });
+    function addKeyupEvent(key, element){
+      if(!element){ return; };
+      element.addEventListener("keyup", function(){
+        addToStorageValue(key, element.value)
+      });
+    };
+
+    function addClickEventSingle(key, element, value){
+      if(!element){ return; };
+      element.addEventListener("click", function(){
+        addToStorageValue(key, value)
+      });
+    }
+
+    function addClickEventArray(key, element, value, array){
+      if(!element){ return; };
+      element.addEventListener("click", function(){
+        addToStorageArray(key, value, array);
+      });
+    };
+
+    function addToStorageValue(key, value){
+      sessionStorage.setItem(key, value);
+    };
+
+    // JSON.stringify() is used here as session storage only supports strings
+    function addToStorageArray(key, value, array){
+      array.push(value);
+      sessionStorage.setItem(key, JSON.stringify(array));
+    };
   };
 
-  function addClickEventSingle(key, element, value){
-    if(!element){ return; };
-    element.addEventListener("click", function(){
-      addToStorageValue(key, value)
+  // Adds a class to baloons which makes them disappear when clicked.
+  if (document.querySelector('.personality')){
+    var balloons = document.querySelectorAll('[class^="balloons--"]');
+
+    balloons.forEach( function (balloon) {
+      balloon.addEventListener('click', function () {
+        this.classList.add('pop');
+      });
     });
   }
-
-  function addClickEventArray(key, element, value, array){
-    if(!element){ return; };
-    element.addEventListener("click", function(){
-      addToStorageArray(key, value, array);
-    });
-  };
-
-  function addToStorageValue(key, value){
-    sessionStorage.setItem(key, value);
-  };
-
-  // JSON.stringify() is used here as session storage only supports strings
-  function addToStorageArray(key, value, array){
-    array.push(value);
-    sessionStorage.setItem(key, JSON.stringify(array));
-  };
-}();
-
-
-// Adds a class to baloons which makes them disappear when clicked.
-if (document.querySelector('.personality')){
-  var balloons = document.querySelectorAll('[class^="balloons--"]');
-
-  balloons.forEach( function (balloon) {
-    balloon.addEventListener('click', function () {
-      this.classList.add('pop');
-    });
-  });
-}
-
-(function ($) {
-  "use strict";
 
   window.updateOutput = function (figure) {
     var max = $(".slider__range").attr("max");
@@ -94,4 +89,5 @@ if (document.querySelector('.personality')){
   }
 
   updateOutput($(".slider__range").val());
-})(jQuery);
+
+})();
