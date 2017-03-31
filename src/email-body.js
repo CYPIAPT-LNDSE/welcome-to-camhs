61c347667c017f385b54dc34e2985b157085df21
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-03-31 22:59:35 +0100

    Format email in email-body.js
    Related #70

--- src/email-body.js
+++ src/email-body.js
@@ -1,20 +1,25 @@
-// const path = require("path");
-// const fs = require("fs");
-// const lionImage = fs.readFileSync(`${path.dirname(__filename)}/../public/assets/lion.svg`, 'utf8');
-// const lionPath = `${path.dirname(__filename)}/../public/assets/lion.svg`;
-// console.log(lionPath);
+const bodyStyle = `background: #60ADD6;
+                   margin: 15px;
+                   padding: 30px;
+                   text-align: center`;
 
-const css = `background: #60ADD6;
-            margin: 15px;
-            padding: 30px;
-            text-align: center`;
+const imgStyle = `width:220px;
+                  height:auto;
+                  margin: 0 auto`;
+
+const lineStyle = `width: 375px;
+                   border: 1px solid #FCF74C;
+                   margin-bottom: 23px`;
+
+const pStyle = `margin: 0`;
+
+const hStyle = `margin: 0 0 23px 0`;
 
 function buildEmail(emailContent){
   const emailBody = JSON.parse(emailContent);
-  const backup = 'no answer given';
 
   function formatArray(array){
-    if(!emailBody.array){ return 'no answer given' };
+    if(!emailBody[array]){ return 'no answer given' };
     let arr = JSON.parse(emailBody[array]);
     return arr.map(word => word.slice(0,1).toUpperCase() + word.slice(1)).join(', ');
   }
@@ -25,29 +30,37 @@ function buildEmail(emailContent){
     return value + '/5';
   }
 
-// http://localhost:8080/assets/lion.svg
-// https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg
-
   return `
-          <div style="${css}">
-            <div style="width:200px; height:200px; margin: 0 auto; border:2px solid black">
-              <img src="${lionPath}">
-            </div>
-            <h1>Getting to know you Questionnaire:</h1>
-            <h3>Name: ${formatValues(emailBody.name)}</h3>
-            <h3>Age: ${formatValues(emailBody.age)}</h3>
-            <h3>Friends: ${formatValues(emailBody.Friends)}</h3>
-            <h3>Friends likes: ${formatValues(emailBody.friends__like)}</h3>
-            <h3>Friends dislikes: ${formatValues(emailBody.friends__dislike)}</h3>
-            <h3>School: ${formatValues(emailBody.School)}</h3>
-            <h3>School likes: ${formatValues(emailBody.school__like)}</h3>
-            <h3>School dislikes: ${formatValues(emailBody.school__dislike)}</h3>
-            <h3>Sleep: ${formatValues(emailBody.Sleep)}</h3>
-            <h3>Personality: ${formatArray('personality')}</h3>
-            <h3>Hobbies: ${formatArray('hobbies')}</h3>
-            <h3>Feelings: ${formatValues(emailBody.feelings)}</h3>
+          <div style="${bodyStyle}">
+            <img style="${imgStyle}" src="https://image.ibb.co/caBmBF/lion.png">
+            <h1>Getting To Know You</h1>
+            <hr style="${lineStyle}">
+            <p style="${pStyle}">Name:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.name)}</h3>
+            <p style="${pStyle}">Age:</p>
+            <h2 style="${hStyle}">${emailBody.age || 'no answer given'}</h3>
+            <p style="${pStyle}">Friends:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.Friends)}</h3>
+            <p style="${pStyle}">Friends likes:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.friends__like)}</h3>
+            <p style="${pStyle}">Friends dislikes:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.friends__dislike)}</h3>
+            <p style="${pStyle}">School:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.School)}</h3>
+            <p style="${pStyle}">School likes:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.school__like)}</h3>
+            <p style="${pStyle}">School dislikes:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.school__dislike)}</h3>
+            <p style="${pStyle}">Sleep:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.Sleep)}</h3>
+            <p style="${pStyle}">Personality:</p>
+            <h2 style="${hStyle}">${formatArray('personality')}</h3>
+            <p style="${pStyle}">Hobbies:</p>
+            <h2 style="${hStyle}">${formatArray('hobbies')}</h3>
+            <p style="${pStyle}">Feelings:</p>
+            <h2 style="${hStyle}">${formatValues(emailBody.feelings)}</h3>
           </div>
-          `;
+        `;
 }
 
 module.exports = { buildEmail };
