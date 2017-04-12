2158a3e2830c325c77882aea4d2d27d586452606
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-04-12 11:57:47 +0100

    Add animation to lion.svg
    Related #163 and #13

--- public/index.js
+++ public/index.js
@@ -84,6 +84,35 @@
     loop: true
   });
 
+  anime({
+    targets: '#lion-head_2_',
+    translateY: 6,
+    duration: 1700,
+    easing: 'easeInQuad',
+    direction: 'alternate',
+    loop: true
+  });
+
+  anime({
+    targets: '#lion-tail',
+    translateY: 1,
+    duration: 3000,
+    easing: 'easeInQuad',
+    direction: 'alternate',
+    rotate: 0.5,
+    loop: true
+  });
+
+  anime({
+    targets: '#lion-tail',
+    translateY: 1,
+    duration: 3000,
+    easing: 'easeInQuad',
+    direction: 'alternate',
+    rotate: 0.5,
+    loop: true
+  });
+
   [
     'lion',
     'bear',
